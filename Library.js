f67b38b01bedc75f9ffa71fffea82e675e8be4c4
Mayur013 <mayurratadiya013@gmail.com>
2024-08-31 20:14:12 +0530

    Add production code for passing isbn format which throws error on invalid format

--- Library.js
+++ Library.js
@@ -7,9 +7,17 @@ class Library{
 }
 
 class AddBooks extends Library{
+    static chk=/^(?:\d{9}[\dX]|\d{1}-\d{4}-\d{4}-[\dX]|\d{13}|\d{1,5}-\d{1,7}-\d{1,7}-\d{1}$|978[-\s]?\d{1,5}[-\s]?\d{1,7}[-\s]?\d{1,7}[-\s]?\d{1}$|979[-\s]?\d{1,5}[-\s]?\d{1,7}[-\s]?\d{1,7}[-\s]?\d{1}$)/;
     addBooks(book){
+        if (!AddBooks.isValidISBN(book.isbn)) {
+            throw new Error('Invalid ISBN format');
+        }
+
         Library.books.push(book);
     } 
+    static isValidISBN(isbn) {
+        return AddBooks.chk.test(isbn) ;
+    }
 }
 
 module.exports = {Library,AddBooks};
